ff2b68d337f63560b61a5f8eaf5e3f66d87ac5b4
JONATHAN HAGEN <jhagen920110@gmail.com>
2017-01-29 04:05:18 -0800

    Create turtleFunctionTest.py

---
+++ turtleFunctionTest.py
@@ -0,0 +1,14 @@
+import turtle
+from TurtleFunctions import *
+
+# Draw a line from (-50, -50) to (50, 50)
+drawLine(-50, -50, 50, 50)
+
+# Write a text at (-50, -60)
+writeText("Testing useful Turtle functions", -50, -60)
+
+# Draw a point at (0, 0)
+drawPoint(0, 0)
+
+# Draw a circle at (0, 0) with radius 80
+drawCircle(0, 0, 80)
